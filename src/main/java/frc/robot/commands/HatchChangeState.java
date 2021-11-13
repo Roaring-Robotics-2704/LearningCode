ef612276afc50a80cfcefa7f6ae9e731f44a30df
annellabellot <artemis011235@gmail.com>
2021-11-13 12:27:51 -0600

    fix broken classes and imports

--- src/main/java/frc/robot/commands/HatchChangeState.java
+++ src/main/java/frc/robot/commands/HatchChangeState.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Hatch;
 
 /** An example command that uses an example subsystem. */
-public class
+public class HatchChangeState extends CommandBase {
     @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
     private final Hatch m_subsystem;
 
